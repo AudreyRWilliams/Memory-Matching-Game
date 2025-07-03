bd7f4d4b8f6d9fd7ffc875faafe116b681d84cd9
Audrey Williams <120662264+AudreyRWilliams@users.noreply.github.com>
2025-07-03 11:11:01 -0400

    Update indexscript.js

--- indexscript.js
+++ indexscript.js
@@ -13,3 +13,16 @@ fetch("./files/gamecards.json")
     shuffleCards();
     generateCards();
   });
+
+function shuffleCards() {
+  let currentIndex = cards.length,
+    randomIndex,
+    temporaryValue;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
+}
